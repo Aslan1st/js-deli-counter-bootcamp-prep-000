292f89959a7e82c41ba0459f61930f0ee107f661
Aslan1st <aslan1st@mac.com>
2017-10-03 03:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0){
     return `The line is currently empty.`
   }else {
+    var theLine = []
     var string = "The line is currently:"
     for (var i = 0; i < katzDeliLine.length; i++){
-    katzDeliLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
+    theLine.push(` ${i + 1}. ${katzDeliLine[i]}`)
     }
     return `${string}${theLine}`
   }
